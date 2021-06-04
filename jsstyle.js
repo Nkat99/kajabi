62c331e2a1d884407fce1446d6db734e77c25e87
Noni K McDonald <72144705+Nkat99@users.noreply.github.com>
2021-06-04 17:07:40 +1000

    Update jsstyle.js

--- jsstyle.js
+++ jsstyle.js
@@ -1,1 +1,12 @@
-if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://nkat99.github.io/kajabi/checkout-hover.css">');
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://nkat99.github.io/kajabi/checkout-hover.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
