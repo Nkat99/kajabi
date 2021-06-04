835626ce0d38860d41e6a6c9be0ef1ff8aaadbd1
Noni K McDonald <72144705+Nkat99@users.noreply.github.com>
2021-06-04 16:59:23 +1000

    Update jsstyle.js

--- jsstyle.js
+++ jsstyle.js
@@ -1,1 +1,1 @@
-if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="/checkout-hover.css">');
+if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="https://nkat99.github.io/kajabi/checkout-hover.css">');
